6385d0d0b38fd2016176b08803a3839f2c66a33e
Anders Haig <anders@minim.co>
2016-07-24 15:06:00 -0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var app = angular.module('starterApp', ['ngMaterial']);
+
+  app.config(function ($mdThemingProvider, $mdIconProvider) {
+    $mdIconProvider
+      .defaultIconSet("./assets/svg/avatars.svg", 128)
+      .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+      .icon("share"      , "./assets/svg/share.svg"       , 24)
+      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+      .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+
+    $mdThemingProvider.theme('default')
+      .primaryPalette('brown')
+      .accentPalette('red');
+  });
